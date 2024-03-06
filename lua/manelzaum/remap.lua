808b3c881ee4eb61c31fd49421127258e38f63f8
Emanuel Alves <manelzaum@icloud.com>
2024-03-06 14:40:09 -0300

    flutter-tools adicionado.

--- lua/manelzaum/remap.lua
+++ lua/manelzaum/remap.lua
@@ -25,3 +25,4 @@ vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/the
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function() vim.cmd("so") end)
+
