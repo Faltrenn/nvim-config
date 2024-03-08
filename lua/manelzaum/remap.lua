61b738cea601ab197c45b84a3518b432c9560320
Emanuel Alves <manelzaum@icloud.com>
2024-03-08 11:38:36 -0300

    Movimentação jk entre wraplines melhorada.

--- lua/manelzaum/remap.lua
+++ lua/manelzaum/remap.lua
@@ -2,6 +2,9 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pq", vim.cmd.Ex)
 
+vim.keymap.set({ "n", "v" }, "j", "gj")
+vim.keymap.set({ "n", "v" }, "k", "gk")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
