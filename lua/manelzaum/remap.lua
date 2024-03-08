0efc9e70c613becb61bbc93cabd5574a50bb2b50
Emanuel Alves <manelzaum@icloud.com>
2024-03-08 16:51:12 -0300

    Atalho removido.

--- lua/manelzaum/remap.lua
+++ lua/manelzaum/remap.lua
@@ -14,8 +14,6 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
